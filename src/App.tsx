c017d5ab2f13aebe91616caae5b086e7482c72fc
gopengglampingpark-maker <gopengglampingpark@gmail.com>
2025-11-28 01:41:44 +0800

    Fix NavBar typo and update API key syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import NavBar from './components/Navbar';
+import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Contact from './components/Contact';
